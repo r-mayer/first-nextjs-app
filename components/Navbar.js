f071f79eeed40287e1e078351c800bd29e144bb8
Rodrigo Mayer <rodrigo-mayer@hotmail.com>
2022-06-01 15:27:31 -0300

    add navItems hover

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
             <div className={styles.navbarContainer}>
                 <ul className={styles.navbarWrapper}>
                     <li className={styles.navItem} name="site"><i class="fa-solid fa-up-right-from-square"></i></li>
-                    <li className={`${styles.navItem} ${styles.principal}`} name="home"><i class="fa-solid fa-house"></i></li>
+                    <li className={`${styles.navItem} ${styles.principal}`} name="HOME"><i class="fa-solid fa-house"></i></li>
                     <li className={styles.navItem} name="github"><i class="fa-brands fa-github"></i></li>
                 </ul>
             </div>    
