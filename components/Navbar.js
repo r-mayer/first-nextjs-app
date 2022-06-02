2ca73ee0ef566319bfc8ed6362a1d0978023299b
Rodrigo Mayer <rodrigo-mayer@hotmail.com>
2022-06-02 19:12:32 -0300

    add external links

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,14 @@
 import styles from './Navbar.module.css'
+import Link from 'next/link'
 
 function Navbar() {
     return(
         <>
             <div className={styles.navbarContainer}>
                 <ul className={styles.navbarWrapper}>
-                    <li className={styles.navItem} name="site"><i class="fa-solid fa-up-right-from-square"></i></li>
-                    <li className={`${styles.navItem} ${styles.principal}`} name="HOME"><i class="fa-solid fa-house"></i></li>
-                    <li className={styles.navItem} name="github"><i class="fa-brands fa-github"></i></li>
+                    <a className={styles.menuLink} href="https://r-mayer.github.io/tractian-react-landing-page/"><li className={styles.navItem} name="site"><i class="fa-solid fa-up-right-from-square"></i></li></a>
+                    <Link href="/"><li className={`${styles.navItem} ${styles.principal}`} name="HOME"><i class="fa-solid fa-house"></i></li></Link>
+                    <a className={styles.menuLink} href="https://github.com/r-mayer"><li className={styles.navItem} name="github"><i class="fa-brands fa-github"></i></li></a>
                 </ul>
             </div>    
         </>
