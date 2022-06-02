a279c742b778e5de07b9f5fbf5a6e79532bb4d1a
Rodrigo Mayer <rodrigo-mayer@hotmail.com>
2022-06-02 20:16:36 -0300

    add margin to container

--- components/layout.js
+++ components/layout.js
@@ -25,7 +25,6 @@ export default function Layout({ children, home }) {
                 <meta name="twitter:card" content="summary_large_image" />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
             </Head>
-            {/* <Navbar /> */}
             <header className={styles.header}>
                 {home ? (
                     <>
